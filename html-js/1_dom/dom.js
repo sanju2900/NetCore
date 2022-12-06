648e8e8603abf79dc7a85a8f6d1dcad87f711035
Sanjana Kumari <58020266+sanju2900@users.noreply.github.com>
2022-12-06 11:49:46 +0530

    Add files via upload

--- html-js/1_dom/dom.js
+++ html-js/1_dom/dom.js
@@ -1,13 +1,26 @@
-let parent = document.getElementById('dom-1');
-let spans = parent.getElementsByTagName(span);
+//single element selector
+let ele = document.getElementById('dom-1');
+let ele2 = document.getElementById('dom-2');
 
-let boyElements = document.getElementsByClassName("byname");
-let girlElements = document.getElementsByClassName("get");
+console.log(ele);
+console.log(ele2);
+//ele.style.color='red';
+//ele.innerText ='hello world';
+let ele1 = ele.getElementsByTagName('span');
+console.log(ele1);
 
-let t = document.querySelector("span, p");
-t; // <p> </p> --> `p` element is above `span`
+let sel = document.querySelector('#dom-1');
 
-e = document.querySelector("span, code");
-e; // <span>sp</span> --> because code element is not available 
+sel = document.querySelector('.get');
+console.log(sel); // <span>sp</span> --> because code element is not available 
+
+
+///Multi element selector
+
+let Elements = document.getElementsByClassName('get');
+let dom2Elements = document.getElementsByClassName('byname');
+
+//let t = document.querySelector("span, p");
+//t; // <p> </p> --> `p` element is above `span`
 let e = document.querySelectorAll("span, p");
 // [p, span]
